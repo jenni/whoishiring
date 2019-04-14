5f37d67379f9aa25d8ae9b190a15eb1aead81dc1
Jen <jen.lijo@zalando.de>
2019-04-14 11:19:38 +0200

    Rollback ui and refactor

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const puppeteer = require('puppeteer');
 
 const { mongoose } = require('./db/mongoose');
 const Company = require('./models/company-model');
+
+const crawl = require('./lib/crawler').crawl;
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -11,66 +12,14 @@ const port = process.env.PORT || 4000;
 app.use(bodyParser.json());
 
 app.get('/', async (req, res) => {
-  const companies = await Company.find();
-  const top10 = companies.splice(0, 10);
+    const companies = await Company.find();
+    const top10 = companies.splice(0, 10);
 
-  res.json(top10);
+    res.json(top10);
 });
 
-const extractValues = () => {
-  const values = document.querySelectorAll('.companies-container .tag-link');
-  const arr = [];
-  values.forEach(v => arr.push(v.innerText));
-
-  return arr;
-};
-
-const cleanUpData = (raw) => {
-  const data = raw.map(d => d.replace(/["'()]/g,''));
-  const pristine = data.map(d => {
-    const obj = {
-      name: '',
-      data: {
-        value: '',
-        date: ''
-      }
-    };
-
-    obj.name = d.split(' ').slice(0, - 1).join(',').replace(/,/g, ' ');
-    obj.data.value = d.split(' ').pop();
-    obj.data.date = new Date();
-
-    return obj;
-  });
-
-  return pristine;
-};
-
-const scrape = async () => {
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
-  const page = await browser.newPage();
-  
-  await page.goto('http://berlinstartupjobs.com/');
-  
-  const content = await page.evaluate(extractValues);
-  const companies = cleanUpData(content);
-
-  await browser.close();
-
-  for (company of companies) {
-    const entry = await Company.findOne({ name: company.name });
-  
-    entry ? 
-    await entry.update({ 
-      $push: { data: company.data } 
-    }) : await Company.create(company);
-  }
-};
-
-scrape();
+crawl();
 
 app.listen(port, () => {
-  console.log(`Server up on port ${port}...`);
+    console.log(`Server up on port ${port}...`);
 });
